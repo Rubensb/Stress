3fa5da565a40f43a2c5a20bdd1a2ab2e93e6ded1
RubensB <ru191999@gmail.com>
2019-05-09 01:09:45 +0200

    Schlafen gehen

--- card.js
+++ card.js
@@ -2,14 +2,20 @@ class Card {
   constructor(p, s) {
     this.pos = p;
     this.suit = s;
-    this.sprite = `card_sprite_${this.suit}.jpg`
+    this.element = document.getElementById(`pos${this.pos}`);
+    this.element.style.backgroundImage = "url('card_sprite_0.svg')";
+  }
+
+  setPosition(pos) {
+    this.pos = pos;
+    this.element = document.getElementById(`pos${this.pos}`);
+    this.element.style.backgroundImage = `url(card_sprite_${this.suit}.svg)`;
+
   }
 
   getPosition() {
     return this.pos;
   }
-
-
 
   getSuit() {
     return this.suit;
--- game.js
+++ game.js
@@ -5,23 +5,79 @@ var table_a = [];
 var table_b = [];
 var active_a = [];
 var active_b = [];
+var loc = [];
+var tempA = undefined;
+var tempB = undefined;
 
 function setup() {
-  for (var i = 0; i < 2; i++) {
+  document.getElementById('pos0').style.visibility = 'hidden';
+  for (var i = 0; i < 10; i++) {
+    loc.push(document.getElementById(`pos${i+1}`))
+  }
+
+  for (let h = 0; h < 2; h++) {
     for (let i = 0; i < 4; i++) {
       for (let j = 2; j < 15; j++) {
         deck.push(new Card(0, j));
-        console.log(j);
       }
     }
   }
 
-  console.log(deck.length);
+  shuffle(deck);
 
-  for (let k = 0; k < deck.length; k++) {
-    document.write(`${deck[k].getSuit()}\n`);
+  for (let i = 0; i < 52; i++) {
+   player_a.push(deck.pop());
+   player_b.push(deck.pop());
   }
 
+  for (let j = 0; j < 4; j++) {
+   table_a.push(player_a.pop());
+   table_b.push(player_b.pop());
+   table_a[j].setPosition(j+1);
+   table_b[j].setPosition(j+7);
+  }
+
+  active_a.push(player_a.pop());
+  active_b.push(player_b.pop());
+
+  active_a[0].setPosition(5);
+  active_b[0].setPosition(6);
+
+  console.log(table_a);
+  console.log(table_b);
+
+
+}
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+function printList(arr) {
+  for (let k = 0; k < arr.length; k++) {
+    document.write(`${arr[k].getSuit()}\n`);
+  }
+}
+
+function checkRules(a, b) {
+  if (this.a == this.b) {
+    return True;
+  } else {
+    return False;
+  }
+}
+
+function holdCard(stack, card) {
+  if (this.stack == 0) {
+    tempA = card;
+  }
+  else if (this.stack == 1) {
+    tempB = card;
+  }
 }
 
 setup();
